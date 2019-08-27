89dac84ba1bfa7cd21411131d102bd57800c416d
Ashutosh Kumar <ashutoshpw@users.noreply.github.com>
2019-08-27 09:20:21 +0530

    Added Google Tag Manager

--- freshchat.js
+++ freshchat.js
@@ -2,3 +2,10 @@ window.fcWidget.init({
   token: "2cc4c65d-cb91-460b-ba11-3dd7628f7389",
   host: "https://wchat.freshchat.com"
 });
+
+// Google Tag Manager
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KXZ39HL');
